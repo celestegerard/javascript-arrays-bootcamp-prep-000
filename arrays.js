98c4bc8a67cb014066ebc6bccda2bd814cdbe0e7
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 20:21:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,8 @@ function addElementToBeginningOfArray(array, element) {
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(array, "") {
-  array.unshift("");
+function destructivelyAddElementToBeginningOfArray(array, "element") {
+  array.unshift("element");
   return array;
 }
 
