5706b288155ada6db7065eae4b720d67ada5d63c
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 20:33:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array = ["foo", ...array];
+  array = ["element", ...array];
   return array;
 }
 
