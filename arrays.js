0205056a98eff9dd0bc7a10ce5724dddae35b045
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 18:17:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   var myArray = ["apples", "oranges"];
-  myArray = [...myArray, "element"];
+  myArray = ["element", ...myArray];
   return myArray;
 }
 
@@ -10,4 +10,14 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   var myArray = ["apples", "oranges"];
   myArray.unshift("element");
   return myArray;
+}
+
+function addElementToEndOfArray(array, element) {
+  var myArray = ["apples", "oranges"];
+  myArray = ["el]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var myArray = ["apples", "bananas"];
+  
 }
