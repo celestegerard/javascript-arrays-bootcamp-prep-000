16981f4d964cc318da0cb58082fa4f60a8c30593
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 18:00:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function AddElementToBeginningOfArray() {
+  var myArray = ["apples", "oranges"];
+  ["bananas", ...myArray];
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  var myArray = ["apples", "oranges"];
+  myArray.unshift("bananas");
+}
