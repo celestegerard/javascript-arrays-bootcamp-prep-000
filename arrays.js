832c52a313130ee49b991a93444120dcf7d0807c
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 20:29:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, 'cookies'];
+  array = [...array, 'cookies'];
   return array;
   }
 
