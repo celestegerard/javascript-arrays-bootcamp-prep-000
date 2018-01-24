457ea528b3ac260116d7e19b368bb19c106f94c1
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 20:35:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, 'cookies'];
+  [...cities, "Philadelphia"]
   return array;
   }
 
