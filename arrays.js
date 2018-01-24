e6ee9ba6274e23c4d7d20afc8fa0d844800799a6
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-24 18:05:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(myArray, element) {
   var myArray = ["apples", "oranges"];
   myArray = [...myArray, "bananas"];
 }
 
-function destructivelyAddElementToBeginningOfArray() {
+function destructivelyAddElementToBeginningOfArray(myArray, element) {
   var myArray = ["apples", "oranges"];
   myArray.unshift("bananas");
 }
